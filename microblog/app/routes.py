61d3f3c83165ed9abd205658f1deb59e9606e5ed
NPodlozhniy <43523651+NPodlozhniy@users.noreply.github.com>
2020-01-12 19:09:42 +0300

    Improve Home page and create opportunity to search users

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -19,24 +19,23 @@ from datetime import datetime
 
 from app.forms import EditProfileForm
 
-@app.route('/')
-@app.route('/index')
+from app.forms import PostForm
+from app.models import Post
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    user = { 'nickname': 'Miguel' }
-    posts = [
-        { 
-            'author': { 'nickname': 'John' }, 
-            'body': 'Beautiful day in Portland!' 
-        },
-        { 
-            'author': { 'nickname': 'Susan' }, 
-            'body': 'The Avengers movie was so cool!' 
-        }
-    ]
-    return render_template("index.html",
-        title = 'Home Page',
-        posts = posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    posts = current_user.followed_posts().all()
+    return render_template('index.html', title='Home Page', form=form,
+                           posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -136,3 +135,9 @@ def unfollow(username):
     db.session.commit()
     flash('You are not following {}.'.format(username))
     return redirect(url_for('user', username=username))
+
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
